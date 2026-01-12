45c2742ba1eb607cca918bea98fea83f9a1b0615
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-01-12 16:29:23 +0100

    fix: handle undefined exports properly
    The return type of esmExports was wrong. To fix it, it not only
    changes it to .size as it might still be wrong, it also checks for
    ESM syntax such as import. That way it is safer to differentiate
    between CJS and ESM in these cases.

---
+++ test/fixtures/side-effect-only.mjs
@@ -0,0 +1,4 @@
+// An ESM module with no exports.
+// Its presence is useful for testing heuristics that attempt to distinguish
+// between CJS and ESM when the module format is unknown.
+import './foo.mjs'
