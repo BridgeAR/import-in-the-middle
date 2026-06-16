8c69717333e82b7ccfac56cfe908328d17f71521
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-06-16 14:35:19 +0200

    chore: drop Datadog license headers from new loader sources
    register-hooks.mjs, register-hooks.d.ts and lib/io.mjs were the new source
    files added in this branch and carried the Datadog Apache-2.0 header. Drop it
    for the same reason as the test files: these are upstream-bound additions and
    the header is noise here.

--- register-hooks.mjs
+++ register-hooks.mjs
@@ -1,7 +1,3 @@
-// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
-//
-// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
-
 import * as module from 'module'
 import { createHook } from './create-hook.mjs'
 
